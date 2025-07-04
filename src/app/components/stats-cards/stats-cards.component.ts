47920a95a5a0bdfdd3c0b26f22430bd7b921c0b1
shibin <shibinraj.chambilora@mavim.com>
2025-07-04 17:22:39 +0530

    all the changes

---
+++ src/app/components/stats-cards/stats-cards.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stats-cards',
+  imports: [],
+  templateUrl: './stats-cards.component.html',
+  styleUrl: './stats-cards.component.scss'
+})
+export class StatsCardsComponent {
+
+}
