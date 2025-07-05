b4246bfd8debc36effa8f0439dc62829c305f0b6
shibin <shibinraj.chambilora@mavim.com>
2025-07-06 00:23:28 +0530

    Implemented basic chart

--- src/app/components/charts/charts.component.ts
+++ src/app/components/charts/charts.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { NgxEchartsModule } from 'ngx-echarts';
 
 @Component({
   selector: 'app-charts',
-  imports: [],
+  standalone: true,
+  imports: [NgxEchartsModule],
   templateUrl: './charts.component.html',
-  styleUrl: './charts.component.scss'
+  styleUrl: './charts.component.scss',
 })
 export class ChartsComponent {
-
+  chartOptions = {
+    title: {
+      text: 'Sales Overview',
+    },
+    tooltip: {},
+    xAxis: {
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {},
+    series: [
+      {
+        name: 'Sales',
+        type: 'bar',
+        data: [10, 22, 28, 43, 49, 62, 73],
+      },
+    ],
+  };
 }
